8cd6bcdfb9602d805ed7514f97f5d054f4a7e6bf
jponly <jpangeli514@gmail.com>
2018-07-07 01:45:17 -0700

    second version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     
     
     
-    //  = once all qustion selections have been clicked press done then answers hide and results appear(possible reset button)
+   
     //  - reload the page to start again (or reset)
 
  // - set variables (nouns) questions, correct answers, incorrect answers, results
@@ -15,13 +15,30 @@ $(document).ready(function() {
 
 //  = Pressing the start button displays the questions
 //  - a timer of x amount of seconds countdown
+
+
   $("#start_btn").click(function(){
     $(".forms").show();
+    
+    $("#start_btn").hide();
+    
 
     
 });
 
 //  - questions can only have one answer clicked at one time, cannot click other buttons once one is clicked, can unclick
+
+
+
+//  = once all qustion selections have been clicked press done then answers hide and results appear(might require possible reset button)
+$("#done_btn").on("click"(function() {
+    $(".forms").hide();
+
+}
+
+
+
+//  - reload the page to start again (or create reset)
 
 
     
