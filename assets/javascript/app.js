1ee08540b83d83c19a74d870c6ccf57e75df3761
jponly <jpangeli514@gmail.com>
2018-07-07 22:31:14 -0700

    updatejuly7th

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,61 +1,47 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
     
-    
-    
-    
-   
-    //  - reload the page to start again (or reset)
 
- // - set variables (nouns) questions, correct answers, incorrect answers, results
-  var answerCorrect = true;
-  var answerIncorrect = false;
-  var number = 121;
-  var intervalId;
-
-//  = Pressing the start button displays the questions
-//  - a timer of x amount of seconds countdown
-
-
-  $("#start_btn").click(function(){
-    $(".forms").show();
-    
+    // - set variables (nouns) questions, correct answers, incorrect answers, results
+    var answerCorrect = true;
+    var answerIncorrect = false;
+ //   var number = 121;
+    var number = 10; 
+    var intervalI = 0;
+    var interval;
 
     
 
-    
-     });
+    // start button on click. When button pressed, button dissappears and reveal timer
+    $("#start_btn").click(function () {
+        $(".forms").show();
+        $("#start_btn").hide();
+        interval = setInterval(function() { 
+            $("#interval_time").text(number - intervalI);
+            console.log('The time is: ' + (number - intervalI));
+            intervalI++;
+            if (intervalI > number) {
+                alert('Times up!');
+                clearInterval(interval);
+                interval = null;
+            }
+        }, 1000)
+        
+    });
 
-//  - questions can only have one answer clicked at one time, cannot click other buttons once one is clicked, can unclick
+// "Done" button will stop timer and reveal correct/incorrect answers or reveal box model with results in percentage(if possible)
 
-
-
-//  = once all qustion selections have been clicked press done then answers hide and results appear(might require possible reset button)
-
-
-
-
-//  - reload the page to start again (or create reset)
-
-
-    
-
-  
-
-
-    // need to push again//
-
-    
-
-
-
-
-
-
-
-
-
+    $('#done_btn').click(function() {
+//        document.forms['MyForm']
+        //   $(".forms").show("a");
+        // $('input[value=a]').hide();
+        //  $( ).css( "color", "green" );
+        $( 'input[value=a]' ).parent().css( "background-color", "1px solid green" );
+           
+    })
 
 
     
+
+    
+
 }); //document ready//
